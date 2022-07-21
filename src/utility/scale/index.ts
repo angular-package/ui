efc21e4ae3bcc74aec56fdd20cc8ce216c1dfdd9
angularpackage <sciborrudnicki@wvvw.dev>
2022-07-21 20:29:35 +0000

    chore(utility/scale/index): reorder exports and add export `AVAILABLE_LEVELS` constant.

--- src/utility/scale/index.ts
+++ src/utility/scale/index.ts
@@ -1,3 +1,10 @@
-export { Levels } from './type/levels.type';
+// Class.
 export { Scale } from './src/scale.class';
+
+// Constant.
+export { AVAILABLE_LEVELS } from './src/available-levels.const';
+
+// Type.
+export { Levels } from './type/levels.type';
+
 // export { ScaleService } from './src/scale.service';
